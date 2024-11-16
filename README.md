a6aa0378fbf0b160ea4abfa410832912812730f7
gabimatatov <gavriel.matatov@veeva.com>
2024-11-16 17:36:15 +0200

    initial app setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Posts
+# Posts App
+API Calls presented in `requests.rest` file 
+
+To run app:
+bash ```
+npm i
+npm run dev
+```
---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express()
+const dotenv = require("dotenv").config()
+const bodyParser = require("body-parser");
+
+const mongoose = require('mongoose')
+
+// Database Connection
+mongoose.connect(process.env.DB_CONNECTION);
+const mongodbConnection = mongoose.connection;
+mongodbConnection.on("error", (error) => console.error(error));
+mongodbConnection.once("open", () => console.log("Successfully Connected to Database"));
+
+// Require Routes
+// const postRoutes = require('./routes/posts');
+
+
+// App configuration
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Authors: Gabi Matatov 322404088 & Gal Ternovski')
+})
+
+//app.use("/posts", postRoutes);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+})
