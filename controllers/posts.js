260c56b500eb1c5085cff9b4f8d0251efde79693
gabimatatov <gavriel.matatov@veeva.com>
2024-11-16 17:55:44 +0200

    created post controller and async functions to each operation

---
+++ controllers/posts.js
@@ -0,0 +1,30 @@
+const Post = require('../models/posts');
+
+
+const getAllPosts = async (req, res) => {
+
+};
+
+const createNewPost = async (req, res) => {
+
+};
+
+const deletePost = async (req, res) => {
+
+};
+
+const getPostByid = async (req, res) => {
+
+};
+
+const updatePost = async (req, res) => {
+
+};
+
+module.exports = {
+    getAllPosts,
+    createNewPost,
+    deletePost,
+    getPostByid,
+    updatePost
+};
